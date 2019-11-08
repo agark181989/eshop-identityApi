691e8462c6b13fbc83d04550a3ed7fdd00a2413a
unknown <r1m1nrao@PUNB-TR04-013.hexa.training.local>
2019-11-08 11:47:42 +0530

    InitialCommit

---
+++ IdentityAPI/Helper/StorageAccountHelper.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityAPI.Helper
+{
+    public class StorageAccountHelper
+    {
+        private string storageConectionString;
+        private CloudStorageAccount storageAccount;
+        private CloudQueueClient queueClient;
+
+        public string StorageConnectionString
+        {
+            get { return storageConectionString; }
+            set
+            {
+                this.storageConectionString = value;
+                storageAccount = CloudStorageAccount.Parse(this.storageConectionString);
+
+            }
+        }
+
+        public async Task SendMessageAsync(string messageText, string queueName)
+        {
+            queueClient = storageAccount.CreateCloudQueueClient();
+            var queue = queueClient.GetQueueReference(queueName);
+            await queue.CreateIfNotExistsAsync();
+
+
+            CloudQueueMessage queueMessage = new CloudQueueMessage(messageText);
+            await queue.AddMessageAsync(queueMessage);
+        }
+    }
+}
---
+++ IdentityAPI/Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityAPI.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace IdentityAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<IdentityDbContext>(config =>
+            {
+                config.UseSqlServer(Configuration.GetConnectionString("IdentityConnection"));
+            });
+
+            services.AddCors(c =>
+            {
+                c.AddDefaultPolicy(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+            services.AddSwaggerGen(options => {
+                options.SwaggerDoc("v1", new Info
+                {
+                    Title = "Identity API",
+                    Description = "Authentication methos for Eshop application",
+                    Version = "1.0",
+                    Contact = new Contact
+                    {
+                        Name = "Rohit Agarwal",
+                        Email = "agarwal.rohit993@gmail.com",
+                        Url = "https://github.com/sonusathyadas"
+                    }
+                });
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCors();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Identity API");
+                c.RoutePrefix = "";
+            });
+
+            InitializeDatabase(app); //initialize the database at the time of project run on the server.
+
+            app.UseMvc();
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                using (var dbContext = serviceScope.ServiceProvider.GetRequiredService<IdentityDbContext>())
+                {
+                    dbContext.Database.Migrate();
+                }
+            }
+        }
+    }
+}
